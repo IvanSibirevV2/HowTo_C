fc73cabc69326508d5bc6ea5f133ce81b6a796cb
qwe <qwe>
2021-01-19 08:47:15 +0300

    ** ** **

---
+++ C++/C_АсинхронныйЗапускФункций/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,50 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <functional>
+#include <future>
+#include "windows.h"
+int main()
+{
+    auto summator = [](int& value) { return ++value; };
+    int sum = 1;
+    for (int i = 0; i < 6; ++i)
+        async(std::launch::async, summator, std::ref(sum));
+
+    std::cout << "sum="<< sum << std::endl;
+    std::cout << "sum=" << std::async(std::launch::async, summator, std::ref(sum)).get() << std::endl;
+    //смысл асинхронных функций в том что вы можете запустить их вычисление
+    //высислять их паралельно
+    //И при этом ожидать в конце через Get метод результат окончания их вычисления
+    auto _func_7 = [](int& value) { Sleep(500);std::cout << "_func_7" << std::endl;value = value + 7; return 7; };
+    auto _func_90 = [](int& value) { Sleep(1000);std::cout << "_func_90" << std::endl;value = value + 90; return 90; };
+    auto _func_97 = [](int& value,auto f1, auto f2)
+    {
+        std::async(std::launch::async, f2, std::ref(value));
+        std::async(std::launch::async, f1, std::ref(value));
+        Sleep(1000);
+        std::cout << "_func_97" << std::endl;
+        return 97; 
+    };
+    std::cout << "_func_97 =" 
+        << std::async(std::launch::async, _func_97, std::ref(sum),_func_7,_func_90).get() 
+        << std::endl
+    ;
+    
+    //std::cout << "sum=" <<  << std::endl;
+    
+    std::cout << "Hello World!\n";
+    system("Pause");
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ C++/C_БлокировкиПотоков/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,83 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include "windows.h"
+//#include <stdio.h>
+//#include <stdarg.h>
+//пРИМЕР БЛОКИРОВКИ ПОТОКОВ
+class container
+{
+    
+    
+public:
+    std::mutex _lock;
+    std::string _str = "Привет";
+    void addrange(std::string __str)
+    {
+        _lock.lock();
+        _str = __str;
+        _lock.unlock();
+    }
+};
+void threadFunction(container& c)
+{
+    for (int i = 0;i < 300;i++)
+    {
+        c.addrange("@@@@@@@@@@");
+        c._lock.lock();
+        Sleep(50);
+        c._lock.unlock();
+        std::cout <<"P1"+ c._str << std::endl;
+    }
+}
+void threadFunction2(container& c)
+{
+    for (int i = 0;i < 300;i++)
+    {
+        c.addrange("!!!!!!!");
+        c._lock.lock();
+        Sleep(50);
+        c._lock.unlock();
+        std::cout << "P2" + c._str << std::endl;
+    }
+}
+/// <summary>
+/// Потоки
+/// https://nuancesprog-ru.turbopages.org/nuancesprog.ru/s/p/5452/
+/// Блокировка потоков
+/// https://habr.com/ru/post/182610/
+/// </summary>
+int main()
+{
+    srand((unsigned int)time(0));
+    container cntr;
+    std::thread t1(threadFunction, std::ref(cntr));
+    std::thread t2(threadFunction2, std::ref(cntr));
+    std::thread t3([](container& _cntr)
+        {
+            
+            for (int i = 0;i < 30;i++)
+            {
+                std::cout << _cntr._str << std::endl;
+            }
+        }, std::ref(cntr));
+    t1.join();
+    t2.join();
+    t3.join();
+    std::cout << "Hello World!\n";
+    system("Pause");
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
